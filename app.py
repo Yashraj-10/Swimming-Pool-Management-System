19e4ba84ee4a400ae9d31ec3b82b8c991dbfefe3
Yashraj-10 <yashraj.singh@devrev.ai>
2024-09-21 15:58:33 +0530

    lint

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from website import create_app,addAdmin
+from website import create_app, addAdmin
 from website.models import User
-app=create_app()
+
+app = create_app()
 
 if __name__=='__main__':
    app.run(debug=True)
-   
-    
--- website/views.py
+++ website/views.py
@@ -1,9 +1,7 @@
 
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from flask_login import login_user, logout_user, login_required, current_user
-from pkg_resources import Requirement
-from urllib3 import PoolManager
-from . import db,addAdmin
+from . import db, addAdmin
 from .models import User, Competition, Post, Ticket, Course
 from werkzeug.security import generate_password_hash, check_password_hash
 
